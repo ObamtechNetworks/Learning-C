2c4d2ffedc8f79a423d49a330b0f859a6195ec7e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-08-12 06:05:57 +0100

    testing vs code

---
+++ C_vs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * This is my first use case of using 
+ * vscode to write c programs
+ * 
+ * All making sense
+ * main - entry points
+ * 
+ * Return: 0
+*/
+int main(argc, argv)
+{
+	printf("Hello World\n");
+	return (0);
+}
